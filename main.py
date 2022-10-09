ec30f9770e43a62704fff114e2db9042c036a167
Thiago Griebler <thiago.griebler@outlook.com>
2022-10-09 00:42:00 -0300

    Added Functional Print Indicating Character Exists Within the Word

--- main.py
+++ main.py
@@ -5,11 +5,15 @@ counter = 0
 tries = 1
 while tries < 6:
     user = input("Type your answear: ")
+    assert len(user) == 5, "The word must have 5 letters"
     for  counter in range(0,5):
         if user[counter]==ans[counter]:
             print(user[counter]+' ✔')
         else:
-            print(user[counter] + ' ❌')
+            if all([char in ans for char in user[counter] ]) == True:
+                print(user[counter]+' 🟡')
+            else:
+                print(user[counter] + ' ❌')
         counter = counter + 1
     if user == ans:
         print('You Win')
